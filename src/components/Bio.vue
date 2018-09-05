4654fbcf1e2f83eb8f005aec614e8216152bbe7b
mrangel2389 <mrangel2389@gmail.com>
2018-09-04 20:57:31 -0400

    Added fields for bio form entry

---
+++ src/components/Bio.vue
@@ -0,0 +1,68 @@
+<template>
+    <v-form v-model="bio">
+        <v-text-field
+            v-model="playerName"
+            label="Player Name"
+            required
+        ></v-text-field>
+        <v-text-field
+                v-model="characterName"
+                label="Character Name"
+                required
+        ></v-text-field>
+        <v-subheader
+                v-if="item.header"
+                :key="item.header"
+        >
+           Description
+        </v-subheader>
+            <v-text-field
+                    v-model="gender"
+                    label="Character Name"
+                    required
+            ></v-text-field>
+
+        <v-text-field
+                v-model="playerName"
+                label="Player Name"
+                required
+        ></v-text-field>
+        <v-text-field
+                v-model="playerName"
+                label="Player Name"
+                required
+        ></v-text-field>
+    </v-form>
+</template>
+
+<script>
+    import VTextField from "vuetify/src/components/VTextField/VTextField";
+    export default {
+        name: "Bio",
+      data () {
+          return {
+            playerName:'',
+            characterName:'',
+            motivation:'',
+            obligation:'',
+            description: {
+              gender:'',
+              age:0,
+              hair:'',
+              height:0,
+              build:'',
+              eyes:'',
+              notableFeatures:''
+            }
+          }
+      },
+      components: {VTextField}
+    }
+</script>
+
+<style scoped>
+    input {
+        width: 25%;
+        border: 2px solid;
+    }
+</style>
--- src/router.js
+++ src/router.js
@@ -20,6 +20,11 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/create/bio',
+      name: 'bio',
+      component: () => import('./components/Bio.vue')
     }
   ]
 })
