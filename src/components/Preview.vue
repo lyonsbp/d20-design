abe014346325e4ce50b3f79ea6703219fa25eeb4
Ahsley <akennard243@gmail.comm>
2018-09-04 20:45:47 -0400

    created preview component, added preview route

---
+++ src/components/Preview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>hi im {{name}} </p>
+    <v-text-field
+      v-model="text"
+    ></v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            name: 'ashley',
+            text: null
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
